91b2faf926859e961a45110f894f2ed6bda8923f
floor12 <floor12@floor12.net>
2020-05-21 15:31:20 +0200

    Fix if stupid errors in migration.

--- src/migrations/m200521_201442_phone_is_null.php
+++ src/migrations/m200521_201442_phone_is_null.php
@@ -6,11 +6,11 @@ class m200521_201442_phone_is_null extends Migration
 {
     public function up()
     {
-        $this->alterColumn('{{%user}}', 'email', $this->string()->null()->unique()->comment('Email'));
+        $this->alterColumn('{{%user}}', 'phone', $this->string(14)->null()->comment('Телефон'));
     }
 
     public function down()
     {
-        $this->alterColumn('{{%user}}', 'email', $this->string()->notNull()->unique()->comment('Email'));
+        $this->alterColumn('{{%user}}', 'phone', $this->string(14)->notNull()->unique()->comment('Телефон'));
     }
 }
